d155f809c53f484d16647e8750b8d85b7f7cbb9c
a13xkur4y4m1 <dano.alexperqgmail.com>
2023-05-03 12:33:49 -0600

    “servicios”

---
+++ main.js
@@ -0,0 +1,10 @@
+
+console.log("Bienvenido a mi sitio web");
+
+
+alert("¡Bienvenido a mi sitio web!");
+
+const header = document.querySelector("header");
+header.addEventListener("click", function() {
+  header.style.backgroundColor = "#f00";
+});
